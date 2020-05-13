c964b8b4b10dea4c86e0b0c72c723fce2e4db424
Christopher DiStasio <cadistasio@gmail.com>
2020-05-13 14:20:12 -0500

    Creates Assignment6; Assignment6 prints to console

---
+++ pubhub-practice-intro/src/intro/Assignment6.java
@@ -0,0 +1,14 @@
+package intro;
+
+public class Assignment6 {
+
+	public static void main(String[] args) {
+		
+		double[] doubles = { 10.5, 11.5 };
+		
+		double sum = doubles[0] + doubles[1];
+		
+		System.out.println(sum);
+
+	}
+}
