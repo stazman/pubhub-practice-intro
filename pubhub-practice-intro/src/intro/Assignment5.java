f47b332e3f756d996c83779ac982b4d0c98f7d31
Christopher DiStasio <cadistasio@gmail.com>
2020-05-13 13:48:26 -0500

    Creates Assignment5; Assignment5 prints to console

---
+++ pubhub-practice-intro/src/intro/Assignment5.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		
+		String[] guests = { "John", "Jay", "Frank", "TJ", "Alice", "Ted", "Alison", "James", "Liz", "Sid" };
+		
+		System.out.println(guests[1]);
+
+	}
+}
