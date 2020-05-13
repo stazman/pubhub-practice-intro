27da0c0c433b809f870f21d890c97882822a6239
Christopher DiStasio <cadistasio@gmail.com>
2020-05-13 13:30:01 -0500

    Creates Assignment4; 4 prints to console

---
+++ pubhub-practice-intro/src/intro/Assignment4.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		
+		int[] numberArray = new int[5];
+		
+		System.out.println(numberArray.length);
+		
+	}
+}
