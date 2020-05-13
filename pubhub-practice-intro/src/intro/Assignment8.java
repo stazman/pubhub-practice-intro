3c0fb31b6ac040a11b6b449abaecb2bd42a04cc9
Christopher DiStasio <cadistasio@gmail.com>
2020-05-13 14:49:58 -0500

    Creates Assignment8; Assignment8 prints to console

---
+++ pubhub-practice-intro/src/intro/Assignment8.java
@@ -0,0 +1,15 @@
+package intro;
+
+public class Assignment8 {
+
+	public static void main(String[] args) {
+		
+		float[] floats = { (float) 105.678, (float) 222.4871 };
+		
+		float floatSum = floats[0] * floats[1];
+		
+		System.out.println(floatSum);
+
+	}
+
+}
