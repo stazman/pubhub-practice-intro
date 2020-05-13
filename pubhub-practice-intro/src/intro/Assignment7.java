9896ad70258628ccc3b283f9a650c824087e50b5
Christopher DiStasio <cadistasio@gmail.com>
2020-05-13 14:26:43 -0500

    Creates Assignment7; Assignment7 prints to console

---
+++ pubhub-practice-intro/src/intro/Assignment7.java
@@ -0,0 +1,15 @@
+package intro;
+
+public class Assignment7 {
+
+	public static void main(String[] args) {
+		
+		int[] intArray = { 11, 22, 33 };
+		
+		int equation = (intArray[0] - intArray[1]) + intArray[2];
+		
+		System.out.println(equation);
+		
+	}
+
+}
